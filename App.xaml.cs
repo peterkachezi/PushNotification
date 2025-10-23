e4d31ca8b98eae9e51581aeeca98ecea49fae047
Peter <Peter@DESKTOP-7IFTD0I>
2025-10-23 21:15:05 +0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MAUIFirebaseNotification
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+
+        }
+    }
+}
